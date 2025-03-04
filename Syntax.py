bca940f1531d5d86b7913ca281bd497d2f3cbca7
Devikasathyamoorthy <128576774+Devikasathyamoorthy@users.noreply.github.com>
2025-03-04 09:48:53 +0530

    Add files via upload

---
+++ Syntax.py
@@ -0,0 +1,82 @@
+'''
+age=32
+name="Devika"
+print(type(age))
+print(type(name))
+
+# Declare and assiging variables
+age=32
+height=6.1
+Name="Devi"
+is_student=True
+print("age : ",age)
+print("Height : ",height)
+print("Name :",Name)
+
+#Variable names
+first_name ="Devika"
+last_name="Sathyamoorthy"
+
+#case sensitivity 
+name="Devika"
+Name="Sathyamoorthy"
+
+#understanding the variable type
+age=23
+height=6.1
+name="Devika"
+is_student=True
+print(type(is_student))
+
+age=32
+print(type(age))
+age_str=str(age)
+print(age)
+print
+
+
+##Arithemetic operator
+a=10
+b=5
+add_result=a+b
+sub_result=a-b
+div_result=a/b
+sub_result=a*b
+floor_div_result=a//b
+modules_result=a%b
+print(add_result)
+print(sub_result)
+print(sub_result)
+print(div_result)
+print(floor_div_result)
+print(modules_result)
+
+#Comparison operator
+#Equal to 
+str1="Krish"
+str2="Krish"
+print(str1==str2)
+
+#Not equal to 
+print(str1!=str2)
+
+#Greater than
+num1=45
+num2=55
+print(num1>num2)
+
+#less than 
+print(num1<=num2)
+
+#Logical operators
+##AND,NOT,OR
+x=True
+y=True
+result=x and y
+result1=x or y
+result2=not y
+print(result)
+print(result1)
+print(result2)
+'''
+
